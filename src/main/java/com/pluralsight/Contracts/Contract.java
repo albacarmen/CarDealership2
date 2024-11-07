29207318042221f358f455d4ba7e4217d6d2c105
albacarmen <carmenisabelalba@gmail.com>
2024-11-06 22:30:30 -0500

    Setting up Contract class

--- src/main/java/com/pluralsight/Contracts/Contract.java
+++ src/main/java/com/pluralsight/Contracts/Contract.java
@@ -1,4 +1,55 @@
-package com.pluralsight.Contracts;
+import com.pluralsight.Dealership.Vehicle;
 
-public class Contract {
+public abstract class Contract {
+    private Vehicle vehicleSold;
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean financeOption;
+
+    // Getters and Setters
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanceOption() {
+        return financeOption;
+    }
+
+    public void setFinanceOption(boolean financeOption) {
+        this.financeOption = financeOption;
+    }
+
+    // Abstract Methods for Subclasses
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
+
